f43dacae5da78412d609d3421a3b115f1f14700f
Dmitriy betterHell <skkogame@gmail.com>
2022-07-07 11:21:42 +0300

    progress-line added

---
+++ scripts/progress-scroll.js
@@ -0,0 +1,8 @@
+const progressBar = document.querySelector(".progress-line");
+
+document.onscroll = function () {
+  let progressLine =
+    (window.scrollY / (document.body.clientHeight - window.innerHeight)) * 100;
+
+  progressBar.style.width = progressLine + "%";
+};
